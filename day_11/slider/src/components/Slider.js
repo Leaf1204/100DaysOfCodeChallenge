1259f128cacf847350aee3c3fd638fee31448d50
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2021-01-23 22:44:25 -0700

    create component and removed quotes

---
+++ day_11/slider/src/components/Slider.js
@@ -0,0 +1,175 @@
+import React, { useState, useRef } from "react";
+import { TweenLite, Power3 } from "gsap";
+
+import leftArrow from "../assets/arrow-left.svg";
+import rightArrow from "../assets/arrow-right.svg";
+
+
+const testimonials = [
+  {
+    name: "Dr. Seuss",
+    title: "Author/Poet",
+    image: `https://i.imgur.com/c4I17bM.jpg`,
+    quote:
+      "The more that you read, the more things you will know. The more that you learn, the more places you'll go."
+  },
+  {
+    name: "Zig Ziglar",
+    title: "Author",
+    image: `https://i.imgur.com/UruLMgf.jpg`,
+    quote:
+      "Positive thinking will let you do everything better than negative thinking will."
+  },
+  {
+    name: "Les Brown",
+    title: "Author",
+    image: `https://i.imgur.com/sKnSRla.jpg`,
+    quote:
+      "If opportunity doesn’t knock, build a door."
+  }
+];
+
+
+function Slider() {
+  let imageList = useRef(null);
+  let testimonialList = useRef(null);
+  const imageWidth = 340;
+
+  const [state, setState] = useState({
+    isActive1: true,
+    isActive2: false,
+    isActive3: false
+  });
+
+
+  //Image transition
+  const slideLeft = (index, duration, multiplied = 1) => {
+    TweenLite.to(imageList.children[index], duration, {
+      x: -imageWidth * multiplied,
+      ease: Power3.easeOut
+    });
+  };
+
+  const slideRight = (index, duration, multiplied = 1) => {
+    TweenLite.to(imageList.children[index], duration, {
+      x: imageWidth * multiplied,
+      ease: Power3.easeOut
+    });
+  };
+
+  const scale = (index, duration) => {
+    TweenLite.from(imageList.children[index], duration, {
+      scale: 1.2,
+      ease: Power3.easeOut
+    });
+  };
+
+  //Content transition
+
+  const fadeOut = (index, duration) => {
+    TweenLite.to(testimonialList.children[index], duration, {
+      opacity: 0
+    });
+  };
+
+  const fadeIn = (index, duration) => {
+    TweenLite.to(testimonialList.children[index], duration, {
+      opacity: 1,
+      delay: 1
+    });
+  };
+
+  const nextSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive2: true });
+      //Image transition
+      slideLeft(0, 1);
+      slideLeft(1, 1);
+      scale(1, 1);
+      slideLeft(2, 1);
+      slideLeft(2, 0);
+   
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive3: true });
+      //Image transition
+      slideRight(0, 1);
+      slideLeft(1, 1, 2);
+      slideLeft(2, 1, 2);
+      scale(2, 1);
+
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive1: true, isActive3: false });
+      //Image transition
+      slideLeft(2, 1, 3);
+      slideLeft(0, 1, 0);
+      slideLeft(1, 0, 0);
+      scale(0, 1);
+  
+    }
+  };
+
+  const prevSlide = () => {
+    if (imageList.children[0].classList.contains("active")) {
+      setState({ isActive1: false, isActive3: true });
+      //Image transition
+      slideLeft(2, 0, 3);
+      slideLeft(2, 1, 2);
+      scale(2, 1);
+      slideRight(0, 1);
+      slideRight(1, 1);
+      //content transtion
+   
+    } else if (imageList.children[1].classList.contains("active")) {
+      setState({ isActive2: false, isActive1: true });
+      //Image transition
+      slideLeft(0, 0);
+      slideRight(0, 1, 0);
+      slideRight(1, 1, 0);
+      slideRight(2, 1, 2);
+      scale(0, 1);
+      //content transtion
+    
+    } else if (imageList.children[2].classList.contains("active")) {
+      setState({ isActive2: true, isActive3: false });
+      slideLeft(2, 1);
+      slideLeft(1, 0, 2);
+      slideLeft(1, 1);
+      scale(1, 1);
+      //content transtion
+    
+    }
+  };
+
+  return (
+    <div className="testimonial-section">
+      <div className="testimonial-container">
+        <div onClick={prevSlide} className="arrows left">
+          <span>
+            <img src={leftArrow} alt="left arrow" />
+          </span>
+        </div>
+        <div className="inner">
+          <div className="t-image">
+            <ul ref={el => (imageList = el)}>
+              <li className={state.isActive1 ? "active" : ""}>
+                <img src={testimonials[0].image} alt={testimonials[0].name} />
+              </li>
+              <li className={state.isActive2 ? "active" : ""}>
+                <img src={testimonials[1].image} alt={testimonials[0].name} />
+              </li>
+              <li className={state.isActive3 ? "active" : ""}>
+                <img src={testimonials[2].image} alt={testimonials[0].name} />
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <div className="arrows right" onClick={nextSlide}>
+          <img src={rightArrow} alt="right arrow" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Slider;

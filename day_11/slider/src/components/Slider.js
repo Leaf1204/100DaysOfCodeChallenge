f0dde9ea82072553b51b4de395600a6dc268d8f5
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2021-01-26 23:07:06 -0700

    animation frames not fully functional as they are going through image array too fast

--- day_11/slider/src/components/Slider.js
+++ day_11/slider/src/components/Slider.js
@@ -1,46 +1,42 @@
 import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import { Draggable } from "gsap/Draggable";
-//import ReactDOM from 'react-dom';
 
 import leftArrow from "../assets/arrow-left.svg";
 import rightArrow from "../assets/arrow-right.svg";
 
 gsap.registerPlugin(Draggable);
 
-const testimonials = [
+const images = [
   {
-    name: "image1",
-    title: "Author/Poet",
+    id: "1",
     image: `https://i.imgur.com/eClRomj.png`,
-    quote:
+    caption:
       "The more that you read, the more things you will know. The more that you learn, the more places you'll go."
   },
   {
-    name: "image2",
+    id: "2",
     image: `https://i.imgur.com/EuZiVwU.png`,
-    quote:
+    caption:
       "Positive thinking will let you do everything better than negative thinking will."
   },
   {
-    name: "image3",
+    id: "3",
     image: `https://i.imgur.com/wx36biP.png`,
-    quote:
+    caption:
       "If opportunity doesn’t knock, build a door."
   },
   {
-    name: "image4",
+    id: "4",
     image: `https://i.imgur.com/oSFa6vM.png`,
-    quote:
+    caption:
     "You always pass failure on the way to success."
   }
 ];
 
 
-var numSlides = testimonials.length;
-console.log(numSlides);
+var numSlides = images.length;
 const proxy = document.createElement("div");
-//ReactDOM.render(<div/>, document.getElementById('root'));
 var currentIndex = 0;
 
 function Slider() {
@@ -52,6 +48,20 @@ function Slider() {
   var wrap = gsap.utils.wrap(-100, (numSlides - 1) * 100);
   var slideAnimation = gsap.to({}, {});
   
+  function animateSlides(direction) {
+  
+    var snapX = gsap.utils.snap(sliderRef.current[currentIndex].offsetWidth);
+    var x = snapX(gsap.getProperty(proxy, "x") + direction * sliderRef.current[currentIndex].offsetWidth); 
+    slideAnimation = gsap.to(proxy, {
+      x: x,
+      duration: slideDuration,
+      onUpdate: updateProgress
+    }); 
+
+    // how do I stop the slides from continuing beyond the 1?
+    //slideAnimation.kill();
+  }
+
   useEffect(() => {
     var norm = (gsap.getProperty(proxy, "x") / sliderRef.current[currentIndex].offsetWidth * numSlides) || 0;
     gsap.set(proxy, {
@@ -69,40 +79,20 @@ function Slider() {
   },[]);
   
   const nextSlide =  () => {
-    console.log('next slide');
     animateSlides(-1);
   }
   
   const prevSlide = () => {
-    console.log('prev slide');
     animateSlides(1);
   }
   
   
-  
+
   const addToRefs = (el) =>{
     if(el && !sliderRef.current.includes(el)){
       sliderRef.current.push(el);
-      console.log(el);
     }
   }
-function animateSlides(direction) {
-  
-  // todo : your array is empty, but why?
-  console.log(sliderRef.current[currentIndex].offsetWidth);
-  slideAnimation.kill();
-  var snapX = gsap.utils.snap(sliderRef.current[currentIndex].offsetWidth);
-  var x = snapX(gsap.getProperty(proxy, "x") + direction * sliderRef.current[currentIndex].offsetWidth); 
-  slideAnimation = gsap.to(proxy, {
-    x: x,
-    duration: slideDuration,
-    onUpdate: updateProgress
-  }); 
-
- 
-  
-  
-}
 
 function updateProgress() { 
     gsap.to(sliderRef.current, {
@@ -129,7 +119,7 @@ function updateProgress() {
           <div className="t-image">
             <ul> 
             {
-              testimonials ? testimonials.map((t, index) => (
+              images ? images.map((t, index) => (
                 <li key={index}>
                   <img src={t.image} className='slide' ref={addToRefs} alt=""></img>
                 </li>
